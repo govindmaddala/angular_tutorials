c651f2e8b05fc2d78f8a86363a401084d1dc8631
govind maddala <govindmvn@gmail.com>
2024-12-07 16:57:22 +0530

    component is created manually

---
+++ src/app/_1_ManualComponent/_1_ManualComponent.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "app-_1_ManualComponent",
+    template: "<h2>_1_ManualComponent Template</h2>",
+    styles: ['h2{color: red}']
+})
+export default class _1_ManualComponent{
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import _1_ManualComponent from './_1_ManualComponent/_1_ManualComponent.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, _1_ManualComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
