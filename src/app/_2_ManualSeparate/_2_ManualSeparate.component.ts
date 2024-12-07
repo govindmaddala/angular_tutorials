4a7c719f16c23b44f76aa63f9d4cd57558e5443a
govind maddala <govindmvn@gmail.com>
2024-12-07 17:05:58 +0530

    _2_ManualSeparate i.e separate template and styleurls are added

---
+++ src/app/_2_ManualSeparate/_2_ManualSeparate.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "app_2_ManualSeparate",
+    templateUrl: "./_2_ManualSeparate.component.html",
+    styleUrls: ["./_2_ManualSeparate.component.css"]
+})
+export default class _2_ManualSeparate{
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import _1_ManualComponent from './_1_ManualComponent/_1_ManualComponent.component';
+import _2_ManualSeparate from './_2_ManualSeparate/_2_ManualSeparate.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, _1_ManualComponent],
+  imports: [RouterOutlet, _1_ManualComponent, _2_ManualSeparate],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
